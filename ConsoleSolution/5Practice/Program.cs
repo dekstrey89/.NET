2c7e4c10723363bb74b8216e4b7b8bb01f33cd0d
dekstrey89 <git.dekstrey89@gmail.com>
2017-05-06 16:01:35 +0400

    Добавлен метод, определяющий разрешенные действия для указанного права доступа.

--- ConsoleSolution/5Practice/Program.cs
+++ ConsoleSolution/5Practice/Program.cs
@@ -6,10 +6,91 @@ using System.Threading.Tasks;
 
 namespace _5Practice
 {
+  /// <summary>
+  /// Тип прав.
+  /// </summary>
+  [Flags, Serializable]
+  public enum AccessRights : byte
+  {
+    /// <summary>
+    /// Просмотр.
+    /// </summary>
+    View = 1,
+
+    /// <summary>
+    /// Выполнение.
+    /// </summary>
+    Run = 2,
+
+    /// <summary>
+    /// Добавление.
+    /// </summary>
+    Add = 4,
+
+    /// <summary>
+    /// Изменение.
+    /// </summary>
+    Edit = 8,
+
+    /// <summary>
+    /// Утверждение.
+    /// </summary>
+    Ratify = 16,
+
+    /// <summary>
+    /// Удаление.
+    /// </summary>
+    Delete = 32,
+
+    /// <summary>
+    /// Нет доступа.
+    /// </summary>
+    /// <remarks>
+    /// Этот флаг имеет максимальный приоритет.
+    /// Если он установлен, остальные флаги игнорируются 
+    /// </remarks>
+    AccessDenied = 64
+  }
+
   class Program
   {
+    /// <summary>
+    /// Метод, показывающий перечень разрешенных действий для указанного типа прав доступа.
+    /// </summary>
+    /// <param name="right">Право доступа.</param>
+    private static void PrintAvailableActions(AccessRights right)
+    {
+      if (right == AccessRights.AccessDenied)
+      {
+        Console.WriteLine("Нет разрешенных операций.");
+      }
+      else
+      {
+        foreach (var ar in Enum.GetValues(typeof(AccessRights)))
+        {
+          if ((byte)ar <= (byte)right)
+          {
+            Console.WriteLine(ar);
+          }
+        }
+      }
+    }
+
     static void Main(string[] args)
     {
+      Console.WriteLine("Проверка доступных действий для права {0}:",AccessRights.View);
+      PrintAvailableActions(AccessRights.View);
+
+      Console.WriteLine("\nПроверка доступных действий для права {0}:", AccessRights.Add);
+      PrintAvailableActions(AccessRights.Add);
+
+      Console.WriteLine("\nПроверка доступных действий для права {0}:", AccessRights.Delete);
+      PrintAvailableActions(AccessRights.Delete);
+
+      Console.WriteLine("\nПроверка доступных действий для права {0}:", AccessRights.AccessDenied);
+      PrintAvailableActions(AccessRights.AccessDenied);
+
+      Console.ReadKey();
     }
   }
 }
