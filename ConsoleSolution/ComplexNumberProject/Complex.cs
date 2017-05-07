b13e9e7fb5e99ec16917edd0ef240e276882a9c5
dekstrey89 <git.dekstrey89@gmail.com>
2017-05-07 15:51:05 +0400

    Добавлены комментарии в классе Complex

--- ConsoleSolution/ComplexNumberProject/Complex.cs
+++ ConsoleSolution/ComplexNumberProject/Complex.cs
@@ -7,11 +7,26 @@ using System.Threading.Tasks;
 
 namespace ComplexNumberProject
 {
+  /// <summary>
+  /// Класс "Complex".
+  /// </summary>
   internal class Complex : IComparable
   {
+    /// <summary>
+    /// Действительная часть числа.
+    /// </summary>
     public int Re { get; set; }
+
+    /// <summary>
+    /// Мнимая часть числа.
+    /// </summary>
     public int Im { get; set; }
 
+    /// <summary>
+    /// Реализация IComparable.
+    /// </summary>
+    /// <param name="obj">Объект для сравнения.</param>
+    /// <returns></returns>
     public int CompareTo(object obj)
     {
       if (obj == null) return 1;
@@ -28,6 +43,11 @@ namespace ComplexNumberProject
       }
     }
 
+    /// <summary>
+    /// Вычислить модуль комплексного числа.
+    /// </summary>
+    /// <param name="c">Комплексное число.</param>
+    /// <returns></returns>
     public double CalculateModulus(Complex c)
     {
       return Math.Sqrt(Math.Pow(c.Re, 2) + Math.Pow(c.Im, 2));
