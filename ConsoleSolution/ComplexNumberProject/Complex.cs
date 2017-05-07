036bc9130ec967e927aa9ffc7dfbc0319a482be3
dekstrey89 <git.dekstrey89@gmail.com>
2017-05-07 15:39:21 +0400

    Реализован интерфейс IComparable в классе Complex

---
+++ ConsoleSolution/ComplexNumberProject/Complex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumberProject
+{
+  internal class Complex : IComparable
+  {
+    public int Re { get; set; }
+    public int Im { get; set; }
+
+    public int CompareTo(object obj)
+    {
+      if (obj == null) return 1;
+
+      var other = obj as Complex;
+
+      if (other != null)
+      {
+        return CalculateModulus(this).CompareTo(CalculateModulus(other));
+      }
+      else
+      {
+        throw new ArgumentException("Object is not a Complex");
+      }
+    }
+
+    public double CalculateModulus(Complex c)
+    {
+      return Math.Sqrt(Math.Pow(c.Re, 2) + Math.Pow(c.Im, 2));
+    }
+  }
+}
--- ConsoleSolution/ComplexNumberProject/Program.cs
+++ ConsoleSolution/ComplexNumberProject/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace ComplexNumberProject
 {
-  class Program
+  internal class Program
   {
     static void Main(string[] args)
     {
+      var TwoComplexes = new List<Complex>() { new Complex() { Re = 3, Im = 5 },
+        new Complex() { Re = 4, Im = 4 },
+        new Complex() { Re = 6, Im = 2 },
+        new Complex() { Re = 2, Im = 2 },
+        new Complex() { Re = 4, Im = 2 }};
+
+      Console.WriteLine("Исходная коллекция комплексных чисел:");
+      PrintList(TwoComplexes);
+
+      TwoComplexes.Sort();
+
+      Console.WriteLine("\nСортированная коллекция комплексных чисел:");
+      PrintList(TwoComplexes);
+
       Console.ReadKey();
+    }
+
+    private static void PrintList(List<Complex> list)
+    {
+      foreach (var complex in list)
+      {
+        Console.WriteLine("Complex Re = {0}, Im = {1}, modulus = {2:##.00}", 
+          complex.Re, complex.Im, complex.CalculateModulus(complex));
+      }
     }
   }
 }
