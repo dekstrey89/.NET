9e96be9eba2b2f9984f82b0268edbe025654aad3
dekstrey89 <git.dekstrey89@gmail.com>
2017-05-08 21:58:39 +0400

    Добавлено решение

---
+++ WinFormsSolution/GZipProject/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GZipProject
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void DownloadBtn_Click(object sender, EventArgs e)
+    {
+      using (var sourceStream = new System.IO.FileStream("GZipFileExample.gz", 
+        System.IO.FileMode.Open, System.IO.FileAccess.Read,
+        System.IO.FileShare.Read))
+
+      using (var uncompressedStream = new System.IO.Compression.GZipStream(
+        sourceStream, System.IO.Compression.CompressionMode.Decompress, true))
+
+      using (var textReader = new System.IO.StreamReader(uncompressedStream, true))
+        richTextBox1.Text = textReader.ReadToEnd();
+    }
+  }
+}
---
+++ WinFormsSolution/GZipProject/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GZipProject
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      CreateGZipFile();
+
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+
+    public static void CreateGZipFile()
+    {
+      const string text = "When the going gets tough, the tough get going.\n" +
+                          "Fortune favors the bold.\n" +
+                          "People who live in glass houses should not throw stones.\n" +
+                          "Hope for the best, but prepare for the worst.\n" +
+                          "Better late than never.\n" +
+                          "Discretion is the greater part of valour.\n";
+
+      using (var fileToCompress = new FileStream("GZipFileExample.txt", FileMode.Create))
+      {
+        using (var fileWriter = new StreamWriter(fileToCompress))
+        {
+          fileWriter.Write(text);
+        }
+      }
+
+      using (var compressedFileStream = File.Create("GZipFileExample.gz"))
+      {
+        using (var compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+        {
+          using (var fileToCompress = new FileStream("GZipFileExample.txt", FileMode.Open))
+          {
+            fileToCompress.CopyTo(compressionStream);
+          }
+        }
+      }
+    }
+  }
+}
