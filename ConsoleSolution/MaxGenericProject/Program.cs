e7c49c32be9c1bfe2e0a8cc313cb8e891d1e19b0
dekstrey89 <git.dekstrey89@gmail.com>
2017-05-09 15:24:41 +0400

    Добавлен метод PrintMaximum , который выводит максимум из трех значений любого, но строго одинакового типа.

--- ConsoleSolution/MaxGenericProject/Program.cs
+++ ConsoleSolution/MaxGenericProject/Program.cs
@@ -8,8 +8,37 @@ namespace MaxGenericProject
 {
   class Program
   {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+      var x = 10;
+      var y = 5;
+      var z = 25;
+      PrintMaximum(x, y, z);
+
+      var  date1 = DateTime.Now;
+      var date2 = DateTime.Now.AddDays(5);
+      var date3 = DateTime.Now.AddDays(1);
+      PrintMaximum(date1, date2, date3);
+
+      Console.ReadKey();
+    }
+
+    private static void PrintMaximum<T>(T x, T y, T z)
+      where T : IComparable<T>
+    {
+      var max = x;
+
+      if (y.CompareTo(max) > 0)
+      {
+        max = y;
+      }
+
+      if (z.CompareTo(max) > 0)
+      {
+        max = z;
+      }
+
+      Console.WriteLine("Maximum in set{{ {0}, {1}, {2} }} is {3}\n", x, y, z, max);
     }
   }
 }
