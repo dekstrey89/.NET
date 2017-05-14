011f9d437a306c59ff4d591edabe19f07c274e1e
dekstrey89 <git.dekstrey89@gmail.com>
2017-05-14 23:28:43 +0400

    Добавлены проекты ReflectionProject и DomainProject.

---
+++ ConsoleSolution/DomainProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Remoting;
+
+namespace DomainProject
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Assembly assembly = Assembly.LoadFile("c:/Users/alex/Desktop/.NET/ConsoleSolution/DomainProject/bin/Debug/OldVersion/ReflectionProject.dll");
+      AppDomain domain = AppDomain.CreateDomain("First Domain");
+      string typeName = typeof().FullName;
+
+      ObjectHandle handle = domain.CreateInstance(assembly.GetName().Name, typeName);         
+      var printer = handle.Unwrap() as NewVersion::ReflectionProject.PropertiesPrinter;
+      printer.Print();                                         
+
+      Console.WriteLine("\n\nDone\n\n");
+
+
+      //Assembly assembly2 = Assembly.Load("ReflectionProject2");
+      //AppDomain domain2 = AppDomain.CreateDomain("Second Domain");
+      //string typeName2 = typeof(PropertiesPrinter).FullName;
+
+      //ObjectHandle handle2 = domain2.CreateInstance(assembly2.GetName().Name, typeName2);
+      //var printer2 = handle2.Unwrap() as PropertiesPrinter;
+      //printer2.Print();
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ ConsoleSolution/ReflectionProject/PropertiesPrinter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+//[assembly: AssemblyVersion("1.0.1.0")]
+
+[assembly: AssemblyVersion("1.0.2.0")]
+
+namespace ReflectionProject
+{
+  public class PropertiesPrinter : MarshalByRefObject
+  {
+    /// <summary>
+    /// Возвращает имена всех свойств объекта и строковые представления значений свойств.
+    /// </summary>
+    public void Print()
+    {
+      // First version.
+      //var date = DateTime.Now;
+
+      // Second version.
+      var date = DateTime.Now.AddDays(7.0);
+
+      var type = date.GetType();
+      var properties = type.GetProperties();
+
+      foreach (var property in properties)
+      {
+        Console.WriteLine("{0, 20} {1, 12}:  {2}", property.PropertyType, property.Name, property.GetValue(date));
+      }
+    }
+  }
+}
